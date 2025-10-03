7bdf7645ea14c225125228370c9da0e7ca8f9a68
tedvkremer <tedvkremer@gmail.com>
2025-10-03 10:05:32 -0500

    Refactor carousel and layout styles for improved readability and responsiveness

--- modules/Carousel.js
+++ modules/Carousel.js
@@ -6,12 +6,12 @@ const $$ = (selector, node = document) => node.querySelectorAll(selector);
  *************************************/
 
 export default class Carousel {
-  static NEXT_DURATION = 8000;
+  static DURATION = 8000;
   #id = 0;
   #slides = [];
   #indicators = [];
-  #currSlide = 0;
-  #totSlides = 0;
+  #curr = 0;
+  #total = 0;
   #interval = null;
 
   constructor(id) {
@@ -21,7 +21,7 @@ export default class Carousel {
   init() {
     const carousel = $(this.#id);
     this.#slides = $$('.carousel-slide', carousel);
-    this.#totSlides = this.#slides.length;
+    this.#total = this.#slides.length;
     this.#indicators = $$('.carousel-indicators>button.carousel-dot', carousel);
     this.#indicators.forEach((indicator, i) =>
       indicator.addEventListener('click', () => this.toSlide(i))
@@ -31,14 +31,14 @@ export default class Carousel {
   }
 
   #updateCarousel() {
-    let offset = (0 - this.#currSlide) * 100;
+    let offset = (0 - this.#curr) * 100;
     this.#slides.forEach(slide => {
       slide.style.transform = `translateX(${offset}%)`;
       offset += 100;
     });
 
     this.#indicators.forEach((indicator, i) => {
-      if (this.#currSlide === i) {
+      if (this.#curr === i) {
         indicator.classList.remove('carousel-dot');
         indicator.classList.add('carousel-dot-active');
         indicator.setAttribute('aria-current', 'true');
@@ -52,38 +52,34 @@ export default class Carousel {
 
   nextSlide() {
     this.stopCarousel();
-    this.#currSlide = (this.#currSlide + 1) % this.#totSlides;
+    this.#curr = (this.#curr + 1) % this.#total;
     this.#updateCarousel();
     this.startCarousel();
   }
 
   prevSlide() {
     this.stopCarousel();
-    this.#currSlide = (this.#currSlide - 1 + this.#totSlides) % this.#totSlides;
+    this.#curr = (this.#curr - 1 + this.#total) % this.#total;
     this.#updateCarousel();
     this.startCarousel();
   }
 
   toSlide(index) {
-    if (index < 0 || index >= this.#totSlides) return;
+    if (index < 0 || index >= this.#total) return;
     this.stopCarousel();
-    this.#currSlide = index;
+    this.#curr = index;
     this.#updateCarousel();
     this.startCarousel();
   }
 
   startCarousel() {
-    if (!this.#interval) {
-      this.#interval = setInterval(
-        () => this.nextSlide(),
-        Carousel.NEXT_DURATION);
-    }
+    if (this.#interval) return;
+    this.#interval = setInterval(() => this.nextSlide(), Carousel.DURATION);
   }
 
   stopCarousel() {
-   if (this.#interval) {
-      clearInterval(this.#interval);
-      this.#interval = null;
-    }
+    if (!this.#interval) return;
+    clearInterval(this.#interval);
+    this.#interval = null;
   }
 }
